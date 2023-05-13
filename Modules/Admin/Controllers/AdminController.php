2035ef4e269671529b5f4eab81bc5dee94dc49c8
MITEL\dashmano <manoj.dash@mitel.com>
2023-05-13 10:08:51 +0530

    change in

--- Modules/Admin/Controllers/AdminController.php
+++ Modules/Admin/Controllers/AdminController.php
@@ -18,7 +18,7 @@ class AdminController extends BaseController {
         parent::__construct();
         $openmethods = array('login', 'logout', 'test', 'forgotpassword', 'resetpassword','addressByPincode','getBankDetailByIfsc',
             'getCategoryBySegment','getSubCategoryByCategoryModule','checkpan','checkmobile','checkmobile','checkemail',
-            'getSponsorDetailById','getModuleDetailById');
+            'getSponsorDetailById','getModuleDetailById','sendPendingEmails');
         if (!in_array($this->method, $openmethods)) {
             if ($this->session->has('login')) {
                 if ($this->session->get('usertype') != 4) {
